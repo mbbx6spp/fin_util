b1d1f5fd97d0762b9bb5f9f25f4aeb8e4f0b36b5
Susan Potter <me@susanpotter.net>
2011-12-07 23:57:29 -0600

    Underscore unused inner variable to remove warning

--- src/fin_util.erl
+++ src/fin_util.erl
@@ -20,6 +20,6 @@ generate_passphrase() ->
   generate_passphrase(8).
 
 generate_passphrase(Length) ->
-  lists:flatten(lists:foldl(fun(X, AccIn) ->
+  lists:flatten(lists:foldl(fun(_, AccIn) ->
     [crypto:rand_uniform(32, 122) | AccIn] end,
     [], lists:seq(1, Length))).
